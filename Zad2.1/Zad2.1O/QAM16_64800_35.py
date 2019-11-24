8c20398e2b87326b66f51642ad5399f4eb09614a
siwczak <dawizim@gmail.com>
2019-11-24 18:46:18 +0100

    added new file for QAM16 3/5 and withour 3/5

---
+++ Zad2.1/Zad2.1O/QAM16_64800_35.py
@@ -0,0 +1,52 @@
+import scipy.io as sio
+import math
+
+
+def qam16_35_o(file,LDPC):
+    mat = sio.loadmat(file)
+
+    y= mat['y']
+    yy = y[0, 0]
+
+    v = mat['v']
+    vv = v[0, 0]
+
+    substreams=8
+    nmod=4
+
+
+    V=[0]*LDPC
+
+    for i in range (len(yy)):
+        for j in range (len(yy[i])):
+            if ((i * nmod + j) % substreams == 0):
+                V[0 + math.floor((i * nmod + j) / substreams) * substreams] = yy[i, j]
+
+            if ((i * nmod + j) % substreams == 1):
+                V[2 + math.floor((i * nmod + j) / substreams) * substreams] = yy[i, j]
+            #
+            if ((i * nmod + j) % substreams == 2):
+                V[(3 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+            #
+            if ((i * nmod + j) % substreams == 3):
+                V[(6 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+            #
+            if((i * nmod + j) % substreams == 4):
+                V[(4 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+            #
+            if ((i * nmod + j) % substreams == 5):
+                V[(1 + math.floor((i * nmod + j) / substreams) * substreams)]= yy[i, j]
+            #
+            if ((i * nmod + j) % substreams == 6):
+                V[(7 + math.floor((i * nmod + j) / substreams) * substreams)]= yy[i, j]
+            #
+            if ((i * nmod + j) % substreams == 7):
+                V[(5 + math.floor((i * nmod + j) / substreams) * substreams)]= yy[i, j]
+
+    if(V == vv).all():
+        print("OK")
+    else:
+        print("nie OK")
+
+
+qam16_35_o('demux_16_64800_35.mat',64800)
