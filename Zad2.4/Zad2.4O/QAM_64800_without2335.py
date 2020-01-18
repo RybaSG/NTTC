3df9bf007a294d74f6632b517eeeeebc10ed6ddb
JackoxD <49169245+JackoxD@users.noreply.github.com>
2020-01-18 20:32:01 +0100

    Add files via upload

---
+++ Zad2.4/Zad2.4O/QAM_64800_without2335.py
@@ -0,0 +1,75 @@
+import scipy.io as sio
+import math
+
+
+def qam256_wo2335_o(file, LDPC):
+    mat = sio.loadmat(file)
+
+    y = mat['y']
+    yy = y[0, 0]
+
+    v = mat['v']
+    vv = v[0, 0]
+
+    substreams = 16
+    nmod = 8
+
+    V = [0] * LDPC
+
+    for i in range(len(yy)):
+        for j in range(len(yy[i])):
+            if ((i * nmod + j) % substreams == 0):
+                V[15 + math.floor((i * nmod + j) / substreams) * substreams] = yy[i, j]
+
+            if ((i * nmod + j) % substreams == 1):
+                V[1 + math.floor((i * nmod + j) / substreams) * substreams] = yy[i, j]
+
+            if ((i * nmod + j) % substreams == 2):
+                V[(13 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+            #
+            if ((i * nmod + j) % substreams == 3):
+                V[(3 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+            #
+            if ((i * nmod + j) % substreams == 4):
+                V[(10 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+            #
+            if ((i * nmod + j) % substreams == 5):
+                V[(7 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+            #
+            if ((i * nmod + j) % substreams == 6):
+                V[(9 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+            #
+            if ((i * nmod + j) % substreams == 7):
+                V[(11 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+
+            if ((i * nmod + j) % substreams == 8):
+                V[(4 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+
+            if ((i * nmod + j) % substreams == 9):
+                V[(6 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+
+            if ((i * nmod + j) % substreams == 10):
+                V[(8 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+
+            if ((i * nmod + j) % substreams == 11):
+                V[(5 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+
+            if ((i * nmod + j) % substreams == 12):
+                V[(12 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+
+            if ((i * nmod + j) % substreams == 13):
+                V[(2 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+
+            if ((i * nmod + j) % substreams == 14):
+                V[(14 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+
+            if ((i * nmod + j) % substreams == 15):
+                V[(0 + math.floor((i * nmod + j) / substreams) * substreams)] = yy[i, j]
+
+    if (V == vv).all():
+        print("OK")
+    else:
+        print("nie OK")
+
+
+qam256_wo2335_o('demux_256_64800_without23-35.mat', 64800)
